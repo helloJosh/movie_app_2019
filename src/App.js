63f1449ef9fc0328530f58cd52132c298483e90d
Kim ByungWoo <kimjoshua135@gmail.com>
2023-10-09 23:25:11 +0900

    202301009

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({fav}){
+function Food({name, picutre, rating}){
   console.log(props);
-  return <h1>I like {fav}</h1>;
+  return (
+    <div>
+      <h1>I like {fav}</h1>;
+      <h3>{rating}/5.0</h3>
+      <img src = {picture}/>
+    </div>
+    );
+}
+function renderFood(dish){
+  return <Food name={dish.name} picture={dish.image} rating = {dish.rating}/>;
+}
+
+Food.propTypes ={
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.string.isRequired
 }
 
 const foodILike = [
   {
     name : "1",
-    image : "2"
+    image : "2",
+    rating : "1"
   },
   {
     name : "2",
-    image : "2"
+    image : "2",
+    rating : "1"
   },
   {
     name : "3",
-    image : "2"
+    image : "2",
+    rating : "1"
   },
   {
     name : "4",
-    image : "2"
+    image : "2",
+    rating : "1"
   },
 ]
 
 function App() {
   return( 
     <div>
-      {foodILike.map(dish => <Food name={dish.name}/>)}
+      {foodILike.map(dish => <Food name={dish.name} picture ={dish.image} rating = {dish.rating}/>)}
     </div> 
   
   );
