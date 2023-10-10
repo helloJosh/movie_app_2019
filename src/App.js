a6ae2d25e982e2d5a16dd7906ec8afbacef0d7df
Kim ByungWoo <kimjoshua135@gmail.com>
2023-10-10 17:11:46 +0900

    20231010

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({name, picutre, rating}){
-  console.log(props);
-  return (
-    <div>
-      <h1>I like {fav}</h1>;
-      <h3>{rating}/5.0</h3>
-      <img src = {picture}/>
-    </div>
+class App extends React.Component{
+  state = {
+    count: 0
+  }
+  add = () => {
+    this.setState(current => ({count : current.count +1}));
+  };
+  minus = () => {
+    this.setState(current => ({count : current.count -1}));
+  }
+  componentDidMount(){
+    console.log("Component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+  render(){
+    console.log("I'm rendering");
+    return(
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onCLick={this.minus}>Minus</button>
+      </div>
     );
+  }
 }
-function renderFood(dish){
-  return <Food name={dish.name} picture={dish.image} rating = {dish.rating}/>;
-}
-
-Food.propTypes ={
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.string.isRequired
-}
-
-const foodILike = [
-  {
-    name : "1",
-    image : "2",
-    rating : "1"
-  },
-  {
-    name : "2",
-    image : "2",
-    rating : "1"
-  },
-  {
-    name : "3",
-    image : "2",
-    rating : "1"
-  },
-  {
-    name : "4",
-    image : "2",
-    rating : "1"
-  },
-]
-
-function App() {
-  return( 
-    <div>
-      {foodILike.map(dish => <Food name={dish.name} picture ={dish.image} rating = {dish.rating}/>)}
-    </div> 
-  
-  );
-}
-
 export default App;
